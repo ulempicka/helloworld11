8e95c3c69c461d4293ced226e2cd2d5d283d23b9
ulempicka <urszula.lempicka@gmail.com>
2020-06-06 14:46:58 +0200

    commit 1 all files

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String [] args){
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
+        System.out.print("i\nwiecej\ncommitowania");
+
+    }
+}
